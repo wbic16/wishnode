a980dad12637ac8618213a506a790d1a771daba1
Will Bickford <wbic16@gmail.com>
2024-02-21 23:27:26 -0600

    Verified on omega-pi

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+NAME="phextio"
 MODEL=`cat /proc/cpuinfo |grep ^Model |sed 's/^.*: //g'`
 if [ ! "x$MODEL" = "xRaspberry Pi 5 Model B Rev 1.0" ]; then
   echo "Unexpected $MODEL"
@@ -13,3 +14,18 @@ chmod +x init-zram-swapping.sh
 cp init-zram-swapping.sh /usr/bin/init-zram-swapping
 
 /usr/bin/init-zram-swapping
+
+echo "Installing zfs..."
+HAVE_ZFS=`zfs list |grep phextio |wc -l`
+if [ "x$HAVE_ZFS" = "x0" ]; then
+  apt install zfsutils-linux -y
+  zpool create $NAME /dev/sda
+fi
+
+echo "Enabling zfs compression..."
+zfs set compress=lz4 phextio
+zfs list
+
+if [ "x$HAVE_ZFS" = "x1" ]; then
+  echo "Setup Complete."
+fi

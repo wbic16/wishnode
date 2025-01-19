921eeac038f2992ceb67d14de4463a4cccf5f320
Will Bickford <wbic16@gmail.com>
2025-01-18 18:44:36 -0600

    Fix detection for tinyllama

--- setup-tinyllama.sh
+++ setup-tinyllama.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -d /usr/share/ollama ]; then
   cd /usr/share/ollama
-  if [ ! -d .ollama ]; then
+  if [ ! -f /usr/share/ollama/.ollama/models/manifests/registry.ollama.ai/library/tinyllama/latest ]; then
     echo "Initializing tinyllama..."
     scp wbic16@logos-prime:/home/wbic16/ollama-init.tar ollama-init.tar
     tar xvf ollama-init.tar
